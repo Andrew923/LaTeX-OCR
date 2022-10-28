dabc3bb3b976b55ca0be4cf45594e71bf4f1daec
Andrew923 <yuandrew923@gmail.com>
2022-10-28 00:58:42 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-# Adapted from https://github.com/kingyiusuen/image-to-latex/blob/main/api/app.py
-
 from http import HTTPStatus
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 from io import BytesIO
 from pix2tex.cli import LatexOCR
 
-model = None
 app = FastAPI(title='pix2tex API')
-
+model = None
 
 def read_imagefile(file) -> Image.Image:
     image = Image.open(BytesIO(file))
     return image
-
 
 @app.on_event('startup')
 async def load_model():
     global model
     if model is None:
         model = LatexOCR()
-
 
 @app.get('/')
 def root():
@@ -32,33 +27,15 @@ def root():
     }
     return response
 
-
 @app.post('/predict/')
 async def predict(file: UploadFile = File(...)) -> str:
-    """Predict the Latex code from an image file.
-
-    Args:
-        file (UploadFile, optional): Image to predict. Defaults to File(...).
-
-    Returns:
-        str: Latex prediction
-    """
     global model
     image = Image.open(file.file)
     return model(image)
 
-
 @app.post('/bytes/')
-async def predict_from_bytes(file: bytes = File(...)) -> str:  # , size: str = Form(...)
-    """Predict the Latex code from a byte array
-
-    Args:
-        file (bytes, optional): Image as byte array. Defaults to File(...).
-
-    Returns:
-        str: Latex prediction
-    """
+async def predict_from_bytes(file: bytes = File(...)) -> str:
     global model
-    #size = tuple(int(a) for a in size.split(','))
     image = Image.open(BytesIO(file))
     return model(image, resize=False)
+
